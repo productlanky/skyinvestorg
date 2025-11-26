000bf72c403f8fd336551bb917d2bf615ba2a38e
productlanky <productlanky@gmail.com>
2025-11-26 03:55:54 -0800

    Update Loading.tsx

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -4,7 +4,7 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 export default function Loading() {
   return (
     <div className='flex w-full h-full grow flex-col flex-1 py-20 items-center justify-center'>
-      <AiOutlineLoading3Quarters className='animate-spin size-20 text-primary' />
+      <AiOutlineLoading3Quarters className='animate-spin size-20 text-brand-500' />
     </div>
   )
 }

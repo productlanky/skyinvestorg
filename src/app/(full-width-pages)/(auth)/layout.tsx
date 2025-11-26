3785d701fd32fb3d81c46ce46e242ca776bb6240
productlanky <productlanky@gmail.com>
2025-11-26 03:48:56 -0800

    Add files via upload

--- src/app/(full-width-pages)/(auth)/layout.tsx
+++ src/app/(full-width-pages)/(auth)/layout.tsx
@@ -1,8 +1,7 @@
 import GridShape from "@/components/common/GridShape";
 import ThemeTogglerTwo from "@/components/common/ThemeTogglerTwo";
 
-import { ThemeProvider } from "@/context/ThemeContext";
-import Image from "next/image";
+import { ThemeProvider } from "@/context/ThemeContext";  
 import Link from "next/link";
 import React from "react";
 
